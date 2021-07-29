bcdb795aa656e9b751569c633e97bb6cc1080477
studiowebvision <info@studiowebvision.be>
2021-07-29 11:27:53 +0200

    publish site

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// window.onload = function(){
+    
+//     const desktopMockup = document.querySelector('.mockup__desktop img').width;
+//     const desktopImg = document.querySelector('.mockup__desktop--overlay');
+    
+//     console.log(desktopImg.style.width = `${desktopMockup}px`);
+// }
