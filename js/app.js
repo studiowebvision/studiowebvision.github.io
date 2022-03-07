b14ea869ae93fef8e0600e850fa9930798cc4fcb
studiowebvision <info@studiowebvision.be>
2022-03-07 11:44:54 +0100

    fix fontawesome fonts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // window.onload = function(){
-    
+
 //     const desktopMockup = document.querySelector('.mockup__desktop img').width;
 //     const desktopImg = document.querySelector('.mockup__desktop--overlay');
-    
+
 //     console.log(desktopImg.style.width = `${desktopMockup}px`);
 // }
+
+const slickSlider = require("slick-carousel");
+
+console.log(slickSlider);
