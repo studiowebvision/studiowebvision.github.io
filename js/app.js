cbbf43f02c200c82326c7fb351054786125a10aa
studiowebvision <info@studiowebvision.be>
2022-03-15 23:27:45 +0100

    update layout

--- js/app.js
+++ js/app.js
@@ -1,11 +1,48 @@
-// window.onload = function(){
+$(function () {
+  $("#projects").slick({
+    dots: false,
+    infinite: false,
+    autoplay: false,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableHeight: true,
+    prevArrow: $("#prevProject"),
+    nextArrow: $("#nextProject"),
 
-//     const desktopMockup = document.querySelector('.mockup__desktop img').width;
-//     const desktopImg = document.querySelector('.mockup__desktop--overlay');
+    responsive: [
+      {
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-//     console.log(desktopImg.style.width = `${desktopMockup}px`);
-// }
+  $("#projects .projects__item").each(function (index) {
+    let key = index + 1;
+    $(this).attr("data-aos", "fade-left");
+    $(this).attr("data-aos-delay", `${key}00`);
+  });
 
-const slickSlider = require("slick-carousel");
-
-console.log(slickSlider);
+  AOS.init();
+});
